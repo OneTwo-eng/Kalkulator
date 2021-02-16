e9d8d782a9703eed1924012d48599c82c1fd15db
Aleksander Kaim <olek.kaim@gmail.com>
2021-02-16 22:24:25 +0100

    new methods added

--- src/main/java/com/example/kalkulator/KalkulatorApplication.java
+++ src/main/java/com/example/kalkulator/KalkulatorApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KalkulatorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KalkulatorApplication.class, args);
+    public void add(int a, int b) {
+        System.out.println(a + b);
+    }
+    public void sub (int a, int b) {
+        System.out.println(a - b);
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(KalkulatorApplication.class, args);
+
+        KalkulatorApplication kalkulatorApplication = new KalkulatorApplication();
+        kalkulatorApplication.add(1 , 5);
+        kalkulatorApplication.sub(5,1);
+    }
 }
